2016ffd78cab51a2e9730ab8f3f5ab847f176248
Hunter Callaway <HunterCallaway@hotmail.com>
2019-06-11 14:00:25 -0600

    Destructured the parameter in the App() function, added form tags and a new input, and continued working on the FormikApp variable.

--- react-forms-with-formik/src/App.js
+++ react-forms-with-formik/src/App.js
@@ -3,23 +3,45 @@ import './App.css';
 import {withFormik} from "formik";
 import Yup from 'yup';
 
-function App() {
+function App({
+	values,
+	handleChange,
+	handleSubmit
+				 }) {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
+        <form onSubmit={handleSubmit}>
            <input
               type ="email"
               name="email"
               placeholder="Email"
+				  value={values.email}
+				  onChange={handleChange}
            />
-        </div>
+			  <input
+				  type ="password"
+				  name="password"
+				  placeholder="Password"
+				  value={values.password}
+				  onChange={handleChange}
+			  />
+			  <button>Submit</button>
+        </form>
       </header>
     </div>
   );
 }
 
 export const FormikApp = withFormik({
-
+	mapPropsToValues({email, password}) {
+		return {
+			email: email || '',
+			password: password || ''
+		}
+	},
+	handleSubmit(values) {
+		console.log(values)
+	}
 })(App);
 
