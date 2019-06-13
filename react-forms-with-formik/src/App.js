58e57be5bddb2e290fff151efdff03382a1ba013
Hunter Callaway <HunterCallaway@hotmail.com>
2019-06-12 20:59:40 -0600

    Imported Field, deleted the props that are no longer in use, changed the input tags to Field, added label tags, and set-up initial values for newsletter and plan.

--- react-forms-with-formik/src/App.js
+++ react-forms-with-formik/src/App.js
@@ -1,34 +1,40 @@
 import React from 'react';
 import './App.css';
-import {withFormik, Form} from "formik";
+import {withFormik, Form, Field} from "formik";
 import Yup from 'yup';
 
 function App({
 	//Destructure the props.
 	//These props are provided by Formik. See the docs for other options:
 	//https://jaredpalmer.com/formik/docs/api/formik#initialvalues-values
-	values,
-	handleChange,
-	handleSubmit
+	values
 				 }) {
   return (
     <div className="App">
       <header className="App-header">
         <Form>
-           <input
+           <Field
               type ="email"
               name="email"
               placeholder="Email"
-				  value={values.email}
-				  onChange={handleChange}
            />
-			  <input
+			  <Field
 				  type ="password"
 				  name="password"
 				  placeholder="Password"
-				  value={values.password}
-				  onChange={handleChange}
 			  />
+			  <label>
+				  <Field
+				  type="checkbox"
+				  name="newsletter"
+				  checked={values.newsletter}
+				  />
+				  Click the box to subscribe to our newsletter!
+			  </label>
+			  <Field component="select" name="plan">
+					<option value="free">Free</option>
+					<option value="premium">Premium</option>
+			  </Field>
 			  <button>Submit</button>
         </Form>
       </header>
@@ -39,10 +45,12 @@ function App({
 //Connect the functional component App to Formik.
 export const FormikApp = withFormik({
 	//Set-up the initial values for the fields.
-	mapPropsToValues({email, password}) {
+	mapPropsToValues({email, password, newsletter, plan}) {
 		return {
 			email: email || '',
-			password: password || ''
+			password: password || '',
+			newsletter: newsletter || false,
+			plan: plan || 'premium'
 		}
 	},
 	//This function executes after the user clicks the 'Submit' button.
