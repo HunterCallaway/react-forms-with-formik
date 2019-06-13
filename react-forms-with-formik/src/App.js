1bf5e1d400459af15b1cf0a8ac63716e36c31c46
Hunter Callaway <HunterCallaway@hotmail.com>
2019-06-12 21:52:37 -0600

    Reformatted the code, updated the Yup import statement, added the errors prop and the object literal, and implemented the validationSchema component.

--- react-forms-with-formik/src/App.js
+++ react-forms-with-formik/src/App.js
@@ -1,45 +1,49 @@
 import React from 'react';
 import './App.css';
 import {withFormik, Form, Field} from "formik";
-import Yup from 'yup';
+import * as Yup from 'yup';
 
 function App({
-	//Destructure the props.
-	//These props are provided by Formik. See the docs for other options:
-	//https://jaredpalmer.com/formik/docs/api/formik#initialvalues-values
-	values
+					 //Destructure the props.
+					 //These props are provided by Formik. See the docs for other options:
+					 //https://jaredpalmer.com/formik/docs/api/formik#initialvalues-values
+					 values,
+					 errors
 				 }) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Form>
-           <Field
-              type ="email"
-              name="email"
-              placeholder="Email"
-           />
-			  <Field
-				  type ="password"
-				  name="password"
-				  placeholder="Password"
-			  />
-			  <label>
-				  <Field
-				  type="checkbox"
-				  name="newsletter"
-				  checked={values.newsletter}
-				  />
-				  Click the box to subscribe to our newsletter!
-			  </label>
-			  <Field component="select" name="plan">
-					<option value="free">Free</option>
-					<option value="premium">Premium</option>
-			  </Field>
-			  <button>Submit</button>
-        </Form>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Form>
+					<div>
+						{ errors.email && <p>{errors.email}</p> }
+						<Field
+							type="email"
+							name="email"
+							placeholder="Email"
+						/>
+					</div>
+					<Field
+						type="password"
+						name="password"
+						placeholder="Password"
+					/>
+					<label>
+						<Field
+							type="checkbox"
+							name="newsletter"
+							checked={values.newsletter}
+						/>
+						Click the box to subscribe to our newsletter!
+					</label>
+					<Field component="select" name="plan">
+						<option value="free">Free</option>
+						<option value="premium">Premium</option>
+					</Field>
+					<button>Submit</button>
+				</Form>
+			</header>
+		</div>
+	);
 }
 
 //Connect the functional component App to Formik.
@@ -53,6 +57,17 @@ export const FormikApp = withFormik({
 			plan: plan || 'premium'
 		}
 	},
+	//This Formik component uses Yup for validation.
+	validationSchema: Yup.object().shape({
+		email: Yup
+			.string()
+			.email()
+			.required(),
+		password: Yup
+			.string()
+			.min(8)
+			.required()
+	}),
 	//This function executes after the user clicks the 'Submit' button.
 	handleSubmit(values) {
 		console.log(values)
